4b80421d5d7a5cb7911d75b8c76950ea1970bc18
Mike <mikewrubel51@gmail.com>
2021-04-06 19:42:44 -0400

    commit tree 3d5c6f4b94636d201c5ceb13538f56d1d3581c6f

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-
 // Write your code here
+let num1 = 31, num2 = 2, multiply = num1 * num2;
+let random = Math.floor(Math.random() * 10 + 1);
+let num3 = 25, num4 = 7, mod = (num3 % num4);
+let max = Math.max(5,10,15,20);
